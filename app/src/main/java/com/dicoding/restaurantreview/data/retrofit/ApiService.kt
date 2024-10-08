756ac60f0f431562f3a158ca775112e9e95f4998
Jordi Irawan <64052045+jrdrwn@users.noreply.github.com>
2024-10-08 13:49:13 +0700

    Codelab Mengambil Data Review Restoran

---
+++ app/src/main/java/com/dicoding/restaurantreview/data/retrofit/ApiService.kt
@@ -0,0 +1,11 @@
+package com.dicoding.restaurantreview.data.retrofit
+
+import com.dicoding.restaurantreview.data.response.RestaurantResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ApiService {
+    @GET("detail/{id}")
+    fun getRestaurant(@Path("id") id: String): Call<RestaurantResponse>
+}
